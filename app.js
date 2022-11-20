9588a4718846247a6d064d14087c4ba3f6eeeea3
nour <nbenmoulehom@gmail.com>
2022-11-20 19:51:57 +0100

    trying to find how to creat event listener for each cell

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const grid = document.querySelector('.grid');
 
-
+let r = 16;
+let c = 16;
+let cells = [c*r];
+console.log(grid)
 function createGrid(r, c){
+  
   grid.style.setProperty('--grid-rows', r);
   grid.style.setProperty('--grid-cols', c);
   for (let i = 0; i < (r * c); i++) {
     let cell = document.createElement("div");
-    cell.innerText = (i + 1);
     grid.appendChild(cell).className = "grid-item";
+    cell.innerText = "hi"
+    cells.push(cell);
   };
 }
 
+for (let i = 0; i < (r * c); i++) {
+  console.log(cells[i])
+}
 
 
 createGrid(16, 16);
